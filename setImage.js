d32df34908899fe869130d700fc2b71784bee250
burton <burtoncrypto@vivaldi.net>
2022-09-17 21:16:21 +0100

    Updating image URL

--- setImage.js
+++ setImage.js
@@ -10,7 +10,7 @@ fs.readdir(__dirname, (err, files) => {
   files.forEach(file => {
     if (path.extname(file) === '.json') {
       const json = JSON.parse(fs.readFileSync(file, { encoding:'utf8', flag:'r' }));
-      json.image = `https://github.com/burtoncrypto/retrodogetest/${path.basename(file, '.json')}.jpg`;
+      json.image = `https://raw.githubusercontent.com/burtoncrypto/retrodogetest/main/${path.basename(file, '.json')}.jpg`;
       fs.writeFileSync(file, JSON.stringify(json, null, 2));
     }
   });
